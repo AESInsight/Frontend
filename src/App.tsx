ccbf8d8f5de13365b972ccc58d4f9c8bf81c21ad
Simon <202306749@post.au.dk>
2025-02-03 15:28:36 +0100

    fix(): use eslint/prettier formatter on all current files

--- src/App.tsx
+++ src/App.tsx
@@ -4,17 +4,17 @@ import DefaultComponent from "./components/pages/fallbackpage";
 import "./index.css";
 
 function App() {
-  return (
-    <span className="text-black text-lg font-bold">
-    <div>
-      <h1 className="text-3xl font-bold">Welcome to AESInsight</h1>
-   <div>
-    <DefaultComponent />
-   </div>
-   <ButtonTest />
-    </div>
-    </span>
-  );
+	return (
+		<span className="text-black text-lg font-bold">
+			<div>
+				<h1 className="text-3xl font-bold">Welcome to AESInsight</h1>
+				<div>
+					<DefaultComponent />
+				</div>
+				<ButtonTest />
+			</div>
+		</span>
+	);
 }
 
 export default App;
