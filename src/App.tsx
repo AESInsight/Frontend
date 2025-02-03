af5b44206013b1b1c906e7177e1254295372d4ae
Simon <202306749@post.au.dk>
2025-02-03 12:03:36 +0100

    feat(): sets up frontend react project

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import "./App.css";
+import "./index.css";
+
+function App() {
+  return (
+    <span className="text-black text-lg font-bold">
+      {" "}
+      Indsæt komponent her!{" "}
+    </span>
+  );
+}
+
+export default App;
