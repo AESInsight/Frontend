503beff0e1b9fa9b3a4bc9116253e2fd89f86dbe
Jeppe <Jeppe@fristed.dk>
2025-03-05 13:49:43 +0100

    fixes build and lint errors

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,10 @@
 import "./App.css";
-import { ButtonTest } from "./components/buttonTest";
 import DefaultComponent from "./components/pages/fallbackpage";
 import "./index.css";
 
 function App() {
 	return (
-		<div className="bg-sky-700 shadow-lg border-b text-center">
+		<div className="bg-sky-700 shadow-lg border text-center">
 			<span>
 				<h1 className="text-3xl text-white mb-4 font-bold">
 					Welcome to AESInsight
@@ -13,13 +12,7 @@ function App() {
 			</span>
 			<div className="bg-white">
 				<div>
-					<div>
-						<div>
-							<DefaultComponent />
-						</div>
-						½
-						<ButtonTest />
-					</div>
+					<DefaultComponent />
 				</div>
 			</div>
 		</div>
