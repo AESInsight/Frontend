7b6f5922879b67bd1a5df28d90871a2764212d1c
Thea1007 <thea@hoeghs.com>
2025-04-29 13:46:08 +0200

    Forhåbenligt sidste rettelser

--- src/lib/loginAPI.ts
+++ src/lib/loginAPI.ts
@@ -1,4 +1,4 @@
-import API_BASE_URL from "@/config";
+import API_BASE_URL from "../config";
 import axios from "axios";
 
 const axiosInstance = axios.create({
@@ -21,7 +21,7 @@ const apiClient = axios.create({
 });
 
 export const postLogin = async (email: string, password: string) => {
-	const response = await apiClient.post("/auth/login", {email, password});
+	const response = await apiClient.post("/Auth/login", {email, password});
 	return response.data;
 };
 
